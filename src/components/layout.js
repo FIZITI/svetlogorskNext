5b31898e368413487b0215bb28dd735b52ae759a
username <bormandeny3@gmail.com>
2023-08-21 15:16:18 +0300

    json

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,28 @@
 import Navbar from "../components/navbar";
 import Side from "../components/side";
-export default function Layout({children}) {
+
+export default function Layout({ children }) {
+  let posts = JSON.parse({
+    "link": "information",
+    "name": "infinf"
+  })
+  // let posts = [
+  //   {
+  //     'link': 'information',
+  //     'name': 'Информация',
+  //   },
+  //   {
+  //     'link': 'history',
+  //     'name': 'История',
+  //   },
+  // ];
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="flex flex-col md:flex-row items-start mx-auto max-w-screen-xl gap px">
         <main className="w-full component-block">{children}</main>
-        <Side/>
+        <Side posts={posts}/>
       </div>
     </>
-  )
+  );
 }
--- src/components/side.js
+++ src/components/side.js
@@ -1,35 +1,20 @@
 import Link from "next/link";
 
-export  default function Side() {
+export default function Side({ posts }) {
   return (
-    <>
-      <aside className="sidebar component-block">
-        <h3 className="text-sm font-semibold tracking-wide text-gray-900 uppercase dark:text-white lg:text-xs">
-          На этой странице
-        </h3>
-        <nav className="sidebar-nav">
-          <ul>
-            <li data-section="information" className="sidebar-nav__li sidebar-nav__active">
-              <Link href="#information">Информация</Link>
+    <aside className="sidebar component-block">
+      <h3 className="text-sm font-semibold tracking-wide text-gray-900 uppercase dark:text-white lg:text-xs">
+        На этой странице
+      </h3>
+      <nav className="sidebar-nav">
+        <ul>
+          {posts.map((item, index) => (
+            <li key={index} className="">
+              <Link href={`#${item.link}`}>{item.name}</Link>
             </li>
-            <li data-section="history" className="sidebar-nav__li">
-              <Link href="#history">История развития</Link>
-            </li>
-            <li data-section="klimat" className="sidebar-nav__li">
-              <Link href="#klimat">Климат</Link>
-            </li>
-            <li data-section="population" className="sidebar-nav__li">
-              <Link href="#population">Население</Link>
-            </li>
-            <li data-section="education" className="sidebar-nav__li">
-              <Link href="#education">Образование</Link>
-            </li>
-            <li data-section="economy" className="sidebar-nav__li">
-              <Link href="#economy">Хозяйственная деятельность</Link>
-            </li>
-          </ul>
-        </nav>
-      </aside>
-    </>
-  )
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
 }
