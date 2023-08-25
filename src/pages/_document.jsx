610a1e0d0411609c26ae88d84febbe0194bcc536
username <bormandeny3@gmail.com>
2023-08-25 15:16:11 +0300

    script

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,18 +1,21 @@
-import {Html, Head, Main, NextScript } from 'next/document'
-// import menuBurger from "../js/menu-burger";
+import {Html, Head, Main, NextScript} from 'next/document'
+// import Script from 'next/script'
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-              {/*<script defer src={colorTheme}></script>*/}
-              {/*<script defer src={nav}></script>*/}
-              {/*<script defer src={menuBurger}></script>*/}
-            </Head>
-            <body>
-            <Main></Main>
-            <NextScript></NextScript>
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head>
+        {/*<Script*/}
+        {/*  src="../js/menu-burger.js"*/}
+        {/*  strategy="beforeInteractive"*/}
+        {/*/>*/}
+        {/*<script defer src={colorTheme}></script>*/}
+        {/*<script defer src={nav}></script>*/}
+      </Head>
+      <body>
+      <Main></Main>
+      <NextScript></NextScript>
+      </body>
+    </Html>
+  )
 }
