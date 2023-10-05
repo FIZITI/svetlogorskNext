53668ff84434056a251803f5c6b03ce8e9011913
username <bormandeny3@gmail.com>
2023-10-05 18:00:48 +0300

    scripts $ router

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,22 +3,37 @@ import Side from "./side";
 import {useRouter} from 'next/router';
 import { useEffect, useState } from 'react';
 
+
 export default function Layout({ currentPage, children}) {
   const router = useRouter();
+  const isSideVisible = router.pathname === '/' || router.pathname === '/history' || router.pathname === '/people' || router.pathname === '/culture' || router.pathname === '/industry' || router.pathname === '/farm' || router.pathname === '/attractions';
 
+  // let jsonData;
+  // if (currentPage === '/') {
+  //   jsonData = a;
+  //   console.log(123)
+  // }
   const [jsonData, setJsonData] = useState(null);
 
+  // currentPage = currentPage[1]
+
+  // const jsonFile = `json/${currentPage}.json`;
   useEffect(() => {
-    const jsonFile = `/src/json/${currentPage}.json`;
+    // Этот код будет выполнен при каждом переходе между "страницами"
+    const currentPage = router.route.split('/').pop();
+    const jsonFile = `json/${currentPage}.json`;
 
     fetch(jsonFile)
       .then((response) => response.json())
       .then((data) => {
         setJsonData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching JSON:', error);
       });
-  }, [currentPage]);
+  }, [router.route]);
 
-  const isSideVisible = router.pathname === '/' || router.pathname === '/history' || router.pathname === '/people' || router.pathname === '/culture' || router.pathname === '/industry' || router.pathname === '/farm' || router.pathname === '/attractions';
+
   return (
     <>
       <Navbar/>
