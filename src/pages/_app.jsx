12953bb9554c104e80c68d751140e934e8ec4826
username <bormandeny3@gmail.com>
2023-10-07 16:30:48 +0300

    scripts $ router

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,9 +8,9 @@ export default function MyApp( {Component, pageProps} ) {
 
     return (
         <Layout currentPage={router.pathname}>
-          <Script src="/js/color-theme.js" strategy="afterInteractive"></Script>
-          <Script src="/js/menu-burger.js" strategy="afterInteractive"></Script>
-          <Script src="/js/nav.js" strategy="afterInteractive"></Script>
+          <Script src="/color-theme.js" strategy="afterInteractive"></Script>
+          <Script src="/menu-burger.js" strategy="afterInteractive"></Script>
+          <Script src="/nav.js" strategy="afterInteractive"></Script>
           <Component {...pageProps} />
         </Layout>
     )
