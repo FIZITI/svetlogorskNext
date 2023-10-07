bac94910c755fb1e5fd100fdcd94ed3f8e276b81
username <bormandeny3@gmail.com>
2023-10-07 16:36:54 +0300

    scripts $ router

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -24,7 +24,6 @@ export default function Layout({ currentPage, children}) {
       });
   }, [router.route]);
 
-
   return (
     <>
       <Navbar/>
--- src/components/side.jsx
+++ src/components/side.jsx
@@ -22,6 +22,5 @@ export default function Side({ posts }) {
         </nav>
       </aside>
     </>
-
   );
 }
